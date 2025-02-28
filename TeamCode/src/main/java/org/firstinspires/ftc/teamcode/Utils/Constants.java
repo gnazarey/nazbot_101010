aace16acdbfbe1546df9b165384c9f06ee3046fc
George Nazarey <gnazarey@gmail.com>
2025-02-28 01:01:26 -0700

    Created the Constant class for Naz bot. - Added the initial comments - Added an Auto section

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Constants.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Utils;
+
+/////////////////////////////////////////////////
+// This is the constant package for Nazbot
+// This class will include subclass for each of
+// the components. They will be separated to keep
+// the project easier to manage.
+//
+// Written By: George Nazarey
+// Date: 2/27/2025
+// Email: george@nazarey.ca
+//
+
+public class Constants {
+
+    // This is the auto component of the class
+    public static class Auto {
+        public static final double MAX_SPEED = 0.3;
+        public static final double XPID_Kp = 0.018;
+        public static final double YPID_Kp = 0.018;
+        public static final double HEADING_Kp = 0.015;
+    }
+}
