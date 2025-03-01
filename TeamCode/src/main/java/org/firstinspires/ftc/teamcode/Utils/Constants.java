f792ee7e217e81fe042cc61ca86ff3ee2ab48330
George Nazarey <gnazarey@gmail.com>
2025-02-28 22:54:48 -0700

    Added the RGB_LIGHT class to set the standard colors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Constants.java
@@ -14,10 +14,26 @@ package org.firstinspires.ftc.teamcode.Utils;
 public class Constants {
 
     // This is the auto component of the class
-    public static class Auto {
+    public static final class Auto {
         public static final double MAX_SPEED = 0.3;
         public static final double XPID_Kp = 0.018;
         public static final double YPID_Kp = 0.018;
         public static final double HEADING_Kp = 0.015;
     }
+    // This is the rgb_light component of the class
+    public static final class RGB_Light {
+        public static final double OFF = 0.0;
+        public static final double BLACK = 0.0;
+        public static final double RED = 0.279;
+        public static final double ORANGE = 0.333;
+        public static final double YELLOW = 0.388;
+        public static final double SAGE = 0.444;
+        public static final double GREEN = 0.5;
+        public static final double AZURE = 0.555;
+        public static final double BLUE =  0.611;
+        public static final double INDIGO = 0.666;
+        public static final double VIOLET = 0.722;
+        public static final double WHITE = 1.000;
+    }
+    // This is the tolerance component of the class
 }
