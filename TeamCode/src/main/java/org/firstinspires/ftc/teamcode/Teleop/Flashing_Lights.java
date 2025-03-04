3d7b643d351ef02c42d0bc60ac5b9ad76701f578
George Nazarey <gnazarey@gmail.com>
2025-03-04 00:03:03 -0700

    Changed flashing lights to actually flash lights

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Flashing_Lights.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Flashing_Lights.java
@@ -2,26 +2,25 @@ package org.firstinspires.ftc.teamcode.Teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.teamcode.SubSystem.RGBLights;
 import org.firstinspires.ftc.teamcode.Utils.Constants;
 
 @TeleOp(name="FlashingLights")
 public class Flashing_Lights extends OpMode {
-    private RGBLights lights = null;
+    private RGBLights lights;
+    private int flasher = 0;
 
     @Override public void init(){
         this.lights = new RGBLights(hardwareMap);
-
-}
+    }
     @Override public void loop(){
-        this.lights.setlRGB(Constants.RGB_Light.AZURE);
-        this.lights.setrRGB(Constants.RGB_Light.BLUE);
-        this.lights.setlRGB(Constants.RGB_Light.RED);
-        this.lights.setrRGB(Constants.RGB_Light.GREEN);
-        this.lights.setlRGB(Constants.RGB_Light.INDIGO);
-        this.lights.setrRGB(Constants.RGB_Light.ORANGE);
-        this.lights.setlRGB(Constants.RGB_Light.VIOLET);
-        this.lights.setrRGB(Constants.RGB_Light.SAGE);
-}
+        flasher++;
+        if ((flasher % 2) == 0){
+            this.lights.setrRGB(Constants.RGB_Light.RED);
+            this.lights.setlRGB(Constants.RGB_Light.WHITE);
+        } else {
+            this.lights.setrRGB(Constants.RGB_Light.WHITE);
+            this.lights.setlRGB(Constants.RGB_Light.RED);
+        }
+    }
 }
