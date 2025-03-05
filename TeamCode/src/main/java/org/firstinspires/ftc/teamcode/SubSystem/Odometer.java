8bc6924afd010a2155f4c7447e0dd2eb135b53c1
gnazarey <george@nazarey.ca>
2025-03-04 23:27:25 -0700

    Added getx, gety, getheading, update and reset to the class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/Odometer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/Odometer.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.SubSystem;
 
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.Utils.Constants;
 import org.firstinspires.ftc.teamcode.Utils.GoBildaPinpointDriver;
 
@@ -14,13 +18,41 @@ import org.firstinspires.ftc.teamcode.Utils.GoBildaPinpointDriver;
 // Email: george@nazarey.ca
 //
 public class Odometer {
-    public GoBildaPinpointDriver odometer;
+    private final GoBildaPinpointDriver odometer;
+    private Pose2D pos;
 
     public Odometer(HardwareMap hwMap) {
-
-        odometer = hwMap.get(GoBildaPinpointDriver.class, "xy-cord");
-        odometer.setOffsets(Constants.Odometer.ODOMETER_X_OFFSET, Constants.Odometer.ODOMETER_Y_OFFSET);
-        odometer.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
-        odometer.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        this.odometer = hwMap.get(GoBildaPinpointDriver.class, "xy-cord");
+        this.init();
     }
+
+    private void init(){
+        this.odometer.setOffsets(Constants.Odometer.ODOMETER_X_OFFSET, Constants.Odometer.ODOMETER_Y_OFFSET);
+        this.odometer.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        this.odometer.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        this.update();
+    }
+
+    public double getHeading(){
+        return this.pos.getHeading(AngleUnit.DEGREES);
+    }
+
+    public double getX(){
+        return this.pos.getX(DistanceUnit.MM);
+    }
+
+    public double getY(){
+        return this.pos.getY(DistanceUnit.MM);
+    }
+
+    public void reset(){
+        this.odometer.resetPosAndIMU();
+    }
+
+    public void update(){
+        this.odometer.update();
+        this.pos = this.odometer.getPosition();
+    }
+
+
 }
