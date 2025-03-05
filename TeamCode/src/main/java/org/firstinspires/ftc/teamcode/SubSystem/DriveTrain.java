45b550b1887abcfa771828d05ab10bc026a87de7
George Nazarey <gnazarey@gmail.com>
2025-03-05 11:41:29 -0700

    Started to create the drive train to allow movement with the robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/DriveTrain.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.SubSystem;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Utils.Constants;
+
 /////////////////////////////////////////////////
 // This is the DriveTrain subsystem. It contains
 // all of the initialization and functionality
@@ -8,5 +17,77 @@ package org.firstinspires.ftc.teamcode.SubSystem;
 // Date: 3/4/2025
 // Email: george@nazarey.ca
 //
+@Config
 public class DriveTrain {
+    // Motor definition
+    private final Motor leftFrontDrive;
+    private final Motor leftRearDrive;
+    private final Motor rightFrontDrive;
+    private final Motor rightRearDrive;
+    // Drive Base
+    MecanumDrive driveBase;
+    // PID Controllers
+    private PIDController headingControl = null;
+    // Odometer
+    private Odometer odometer;
+    // Speed Setting
+    private double xSpeed = 0.0;
+    private double ySpeed = 0.0;
+    // Heading Setting
+    private double heading;
+    private double headingCorrection = 0.0;
+
+    public DriveTrain(HardwareMap hwMap){
+        // Assign the motors
+        this.leftFrontDrive = new Motor(hwMap,"LF");
+        this.leftRearDrive = new Motor(hwMap,"LR");
+        this.rightFrontDrive = new Motor(hwMap,"RF");
+        this.rightRearDrive = new Motor(hwMap,"RR");
+        // Assign the drivebase
+        this.driveBase = new MecanumDrive(this.leftFrontDrive,this.rightFrontDrive,this.leftRearDrive,this.rightRearDrive);
+        // PIDController
+        this.headingControl = new PIDController(Constants.DriveBase.HEADING_Kp,Constants.DriveBase.HEADING_Ki,Constants.DriveBase.HEADING_Kd);
+        // Odometer
+        this.odometer = new Odometer(hwMap);
+        // Call init
+        this.init();
+    }
+
+    public void init(){
+        // Setup Tolerance
+        this.headingControl.setTolerance(Constants.DriveBase.HEADING_TOLERANCE);
+        // Initialize Motors
+        this.leftFrontDrive.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        this.leftRearDrive.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        this.rightFrontDrive.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        this.rightRearDrive.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        // Inverting, since it isn't direct drive
+        this.leftFrontDrive.setInverted(true);
+        this.leftRearDrive.setInverted(true);
+        this.rightFrontDrive.setInverted(true);
+        this.rightRearDrive.setInverted(true);
+        // Odometer Update
+        this.odometer.update();
+        // Heading Setting
+        this.heading = 0.0;
+    }
+
+    public void loop(){
+        // xSpeed is the strafing speed -1.0 to +1.0;
+        // ySpeed is the forward speed -1.0 to +1.0;
+        // headingCorrection speed to turn at -1.0 to +1.0;
+        // heading the direction it is facing 0 - 360;
+        this.driveBase.driveFieldCentric(this.xSpeed,this.ySpeed,this.headingCorrection,this.heading,false);
+    }
+
+    public void move(double xDirection, double yDirection){
+        this.xSpeed = xDirection;
+        this.ySpeed = yDirection;
+    }
+
+    public void stop(){
+        this.xSpeed = 0.0;
+        this.ySpeed = 0.0;
+        this.driveBase.stop();
+    }
 }
