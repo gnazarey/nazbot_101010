a89d160d1e078d9d35e475d216bdfae70a12955e
gnazarey <george@nazarey.ca>
2025-03-04 23:02:53 -0700

    Created the base for the drive train

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/DriveTrain.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.SubSystem;
+/////////////////////////////////////////////////
+// This is the DriveTrain subsystem. It contains
+// all of the initialization and functionality
+// to control the motors
+//
+// Written By: George Nazarey
+// Date: 3/4/2025
+// Email: george@nazarey.ca
+//
+public class DriveTrain {
+}
