caa57498e9c5f60a966210da89ff1592f395693d
gnazarey <george@nazarey.ca>
2025-03-04 23:03:22 -0700

    Created the odometer class for access to the odometer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/Odometer.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.SubSystem;
+
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.Utils.Constants;
+
+/////////////////////////////////////////////////
+// This is the Odometer subsystem it communicates
+// with the odometer and provides functionality
+//
+// Written By: George Nazarey
+// Date: 3/4/2025
+// Email: george@nazarey.ca
+//
+public class Odometer {
+    public GoBildaPinpointDriver odometer;
+
+    public Odometer(HardwareMap hwMap) {
+
+        odometer = hwMap.get(GoBildaPinpointDriver.class, "xy-cord");
+        odometer.setOffsets(Constants.Odometer.ODOMETER_X_OFFSET, Constants.Odometer.ODOMETER_Y_OFFSET);
+        odometer.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        odometer.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+    }
+}
