c7c64ab8e5dd074fd68b140aedabe2ceee1acbcd
George Nazarey <gnazarey@gmail.com>
2025-03-06 14:51:32 -0700

    Added Logging to Odometer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/Odometer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/Odometer.java
@@ -8,6 +8,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.Utils.Constants;
 import org.firstinspires.ftc.teamcode.Utils.GoBildaPinpointDriver;
+import org.firstinspires.ftc.teamcode.Utils.Logger;
+
+import java.util.Locale;
 
 /////////////////////////////////////////////////
 // This is the Odometer subsystem it communicates
@@ -21,44 +24,45 @@ public class Odometer {
     private final GoBildaPinpointDriver odometer;
     private Pose2D pos;
     private Pose2D velocity;
+    private final Logger logger;
 
     public Odometer(HardwareMap hwMap) {
         this.odometer = hwMap.get(GoBildaPinpointDriver.class, "odometer");
+        this.logger = new Logger("odometer.log");
         this.init();
     }
 
     private void init(){
+        // Setup Odometer
         this.odometer.setOffsets(Constants.Odometer.ODOMETER_X_OFFSET, Constants.Odometer.ODOMETER_Y_OFFSET);
         this.odometer.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
         this.odometer.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
-        this.odometer.resetPosAndIMU();
+        // Initialize odometer
+        this.reset();
+        // Update odometer
         this.update();
     }
 
-    public double getHeading(){
-        return this.pos.getHeading(AngleUnit.DEGREES);
-    }
+    public double getHeading(){ return this.pos.getHeading(AngleUnit.DEGREES); }
 
-    public double getX(){
-        return this.pos.getX(DistanceUnit.MM);
-    }
+    public double getX(){ return this.pos.getX(DistanceUnit.MM); }
 
     public double getXSpeed() { return this.velocity.getX(DistanceUnit.MM); }
 
-    public double getY(){
-        return this.pos.getY(DistanceUnit.MM);
-    }
+    public double getY() { return this.pos.getY(DistanceUnit.MM); }
 
     public double getYSpeed() { return this.velocity.getY(DistanceUnit.MM); }
 
-    public void reset(){
-        this.odometer.resetPosAndIMU();
-    }
+    public void reset(){ this.odometer.resetPosAndIMU(); }
 
     public void update(){
         this.odometer.update();
         this.pos = this.odometer.getPosition();
         this.velocity = this.odometer.getVelocity();
+
+        this.logger.writeLog(String.format(Locale.ENGLISH,"%s Heading: %f",this.logger.getTimeStamp(),this.getHeading()));
+        this.logger.writeLog(String.format(Locale.ENGLISH,"%s Position: X: %f Y: %f",this.logger.getTimeStamp(),this.getX(),this.getY()));
+        this.logger.writeLog(String.format(Locale.ENGLISH,"%s Velocity: X: %f Y: %f",this.logger.getTimeStamp(),this.getXSpeed(),this.getYSpeed()));
     }
 
 
