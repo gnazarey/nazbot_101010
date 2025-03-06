9646008a33efcea5b1c8011b2512136149eace32
George Nazarey <gnazarey@gmail.com>
2025-03-06 14:51:46 -0700

    Added a timestamp function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Logger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Logger.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.Utils;
 
 import android.os.Environment;
-
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /////////////////////////////////////////////////
 // This is the logging module.
@@ -14,21 +16,24 @@ import java.io.IOException;
 //
 public class Logger {
 
+    //private final String fileName = "/sdcard/FIRST/data/test.log";
     private final String fileName;
-    private FileWriter logWriter;
+    private PrintStream logWriter;
     private Boolean logging = true;
+    private final SimpleDateFormat dateFormat;
 
     public Logger(String filename){
 
-        this.fileName = String.format("%s/FIRST/data/'%s", Environment.getExternalStorageDirectory().getAbsoluteFile(),filename);
+        this.fileName = String.format("%s/FIRST/data/%s",Environment.getExternalStorageDirectory().getPath(), filename);
+        this.dateFormat = new SimpleDateFormat("yyyy-MM-dd@HH-mm-ss", Locale.US);
         this.init();
     }
 
     private void init(){
         try{
-            this.logWriter = new FileWriter(this.fileName,false);
+            this.logWriter = new PrintStream(this.fileName);
         }
-        catch (IOException e) {
+        catch (FileNotFoundException e) {
             this.logging = false;
         }
     }
@@ -37,33 +42,21 @@ public class Logger {
         return this.logging;
     }
 
-    public int writeLog(String logLine)
-    {
-        int outputCount = 0;
+    public String getTimeStamp(){
+        return this.dateFormat.format(new Date());
+    }
 
+    public void writeLog(String logLine)
+    {
         if (this.logging) {
-            try {
-                this.logWriter.write(logLine);
-                outputCount = logLine.length();
+            this.logWriter.println(logLine);
             }
-            catch (IOException e) {
-                outputCount = -1;
-                this.logging = false;
-            }
-        }
-        return outputCount;
     }
 
     public void stopLog() {
-
         if (this.logging) {
-            try {
-                this.logWriter.flush();
-                this.logWriter.close();
-            }
-            catch (IOException e) {
-                this.logging = false;
-            }
+            this.logWriter.flush();
+            this.logWriter.close();
         }
     }
 }
