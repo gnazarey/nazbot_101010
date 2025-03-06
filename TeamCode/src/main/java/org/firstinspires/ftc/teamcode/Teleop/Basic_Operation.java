405748df2054dbc4dd8dc4c921d2ee1635e68412
George Nazarey <gnazarey@gmail.com>
2025-03-06 14:52:19 -0700

    Saved xController and yController Added Logging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Basic_Operation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Basic_Operation.java
@@ -1,45 +1,59 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 
-import android.app.GameManager;
-
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.SubSystem.DriveTrain;
 import org.firstinspires.ftc.teamcode.SubSystem.Odometer;
 import org.firstinspires.ftc.teamcode.SubSystem.RGBLights;
 import org.firstinspires.ftc.teamcode.Utils.Constants;
+import org.firstinspires.ftc.teamcode.Utils.Logger;
+
+import java.util.Locale;
+
 
 @TeleOp(name="Basic_Op")
 public class Basic_Operation extends OpMode {
     private RGBLights lights;
     private Odometer odometer;
-    private GamepadEx driver = null;
+    private GamepadEx controller1 = null;
     private DriveTrain driveTrain;
-
-    public void init_loop(){
-
-    }
+    private Logger logger;
+    private double xController;
+    private double yController;
 
     public void init(){
         this.lights = new RGBLights(hardwareMap);
         this.odometer =new Odometer(hardwareMap);
-        this.driver = new GamepadEx(gamepad1);
+        this.controller1 = new GamepadEx(gamepad1);
         this.driveTrain = new DriveTrain(hardwareMap);
         // Configure Drive Train
         this.driveTrain.setMaxXSpeed(Constants.DriveBase.MAX_SPEED);
         this.driveTrain.setMaxYSpeed(Constants.DriveBase.MAX_SPEED);
+        // Setup Logging
+        this.logger = new Logger("basic_operations.csv");
     }
 
     @Override public void loop(){
 
         // Look for joystick input
+        this.xController = this.controller1.getLeftX();
+        this.yController = this.controller1.getLeftY();
         // Drive Control First
-        this.driveTrain.move(driver.getLeftX(),driver.getLeftY());
+        this.driveTrain.move(this.xController,this.yController);
         // Run the Loops
+        this.writeLog();
         this.odometer.update();
         this.driveTrain.loop();
+    }
+
+    private void writeLog(){
+        // JoyStick Input
+        this.logger.writeLog(String.format(Locale.ENGLISH,"%s Joystick Input X: %f Y: %f",this.logger.getTimeStamp(),this.xController,this.yController));
+    }
+    public void stop(){
+        this.logger.stopLog();
+        this.driveTrain.stop();
     }
 }
