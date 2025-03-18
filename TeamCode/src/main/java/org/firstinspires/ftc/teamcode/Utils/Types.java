423bd276c1b817301d2b940551922e5d63ef21d6
gnazarey <george@nazarey.ca>
2025-03-18 00:02:11 -0600

    Created Types to keep class types

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Types.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Utils;
+
+public class Types {
+    public static class WayPoint {
+        public double x;
+        public double x_speed = 0.2;
+        public double y;
+        public double y_speed = 0.2;
+        public double facing;
+    }
+}
